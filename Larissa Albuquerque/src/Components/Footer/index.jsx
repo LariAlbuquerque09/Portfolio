a9cd431de897ef5921caf9abd51b7dee1ca1fd0f
LariAlbuquerque09 <105288519+LariAlbuquerque09@users.noreply.github.com>
2023-07-16 16:58:10 -0400

    Add files via upload

---
+++ Larissa Albuquerque/src/Components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            Feito com ❣️ por Larissa Albuquerque © 2023
+        </footer>
+    )
+}
+
+export default Footer
